4cc9402361b1326deda95daf1d73c89dbf6637cd
Arooba-Iqbal <aroobaiqbal1058@gmail.com>
2024-11-24 17:29:19 +0500

    Preview of a post

--- app/src/main/java/com/mwafaimk/unify/ui/components/PostCard.kt
+++ app/src/main/java/com/mwafaimk/unify/ui/components/PostCard.kt
@@ -61,19 +61,3 @@ fun PostCard(postData: PostData) {
 
 
 
-@Preview(showBackground = true)
-@Composable
-fun PostCardPreview() {
-    val postData = PostData(
-        profileImage = R.drawable.cat1,
-        userName = "Meow",
-        userEmail = "l226824@lhr.nu.edu.pk",
-        eventTitle = "Anime Watch Party",
-        eventDescription = "Watching Haikyu in CS13\nMaray lea bhi koi Snacks pakr lana",
-        eventTime = "3:30 - 4:50",
-        additionalInfo = "Jitnay zyada utna acha"
-    )
-    MaterialTheme {
-        PostCard(postData = postData)
-    }
-}
